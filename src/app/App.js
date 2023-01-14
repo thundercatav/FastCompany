31f8910204db2ca341057c1f4ca54f3efae583f2
Alexei Verevkin <alex-verevkin@mail.ru>
2023-01-14 18:07:18 +0300

    some improvments

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,7 @@
 import React, { useState } from "react";
 import Users from "./components/users";
-import api from "./api";
 
-function App() {
-  const [users, setUsers] = useState(api.users.fetchAll());
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId));
-  };
-  const handleToggleBookMark = (id) => {
-    setUsers(
-      users.map((user) => {
-        if (user._id === id) {
-          return { ...user, bookmark: !user.bookmark };
-        }
-        return user;
-      })
-    );
-  };
+function App({ users, handleDelete, handleToggleBookMark }) {
   return (
     <div>
       <Users
